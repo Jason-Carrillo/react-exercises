581830d1a969b6405ea33b6f7de3cfe1778979db
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-25 10:55:58 -0700

    added Person import from person.js and displayed it with the rest of the text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className={App}>
         <h1>Hi, I'm Jason Carrillo</h1>
         <p>I am a Software Developer</p>
+        <Person />
     </div>
 
       // After nesting for a long time it becomes too much. Better to not use this way.
