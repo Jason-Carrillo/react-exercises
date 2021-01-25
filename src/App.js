61d6ef2b73680da6568420f0e30a12f31fb5f974
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-25 11:17:45 -0700

    added state that contains all the people and passing the values to the return statement

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Person from './Person/Person';
 
 function App() {
+    let state = {
+        persons: [
+            { name: 'Jason', age: 20},
+            { name: 'Test1', age: 22},
+            { name: 'Test2', age: 18}
+        ]
+    }
   return (
     <div className={App}>
         <h1>Hi, I'm Jason Carrillo</h1>
         <p>I am a Software Developer</p>
-        <Person name="Jason" age="27" />
-        <Person name="Test1" age="20" >My Hobbies: Fishing</Person>
-        <Person name="Test2" age="25" />
+        <Person name={state.persons[0].name} age={state.persons[0].age} />
+        <Person name={state.persons[1].name} age={state.persons[1].age}>My Hobbies: Fishing</Person>
+        <Person name={state.persons[2].age} age={state.persons[2].age} />
     </div>
 
       // After nesting for a long time it becomes too much. Better to not use this way.
