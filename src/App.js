512a4853b787d80693a4b29cd911f93da47f7533
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-25 11:04:07 -0700

    added propsname, ane props age to person function

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className={App}>
         <h1>Hi, I'm Jason Carrillo</h1>
         <p>I am a Software Developer</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Jason" age="27" />
+        <Person name="Test1" age="20" >My Hobbies: Fishing</Person>
+        <Person name="Test2" age="25" />
     </div>
 
       // After nesting for a long time it becomes too much. Better to not use this way.
