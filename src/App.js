8b03f85a939777d07735643df56696f9e463bfc3
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-25 10:43:28 -0700

    tested different ways to display information and apply tags using React

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className={App}>
-      <h1>Hi, I'm Jason Carrillo</h1>
+        <h1>Hi, I'm Jason Carrillo</h1>
+        <p>I am a Software Developer</p>
     </div>
+
+      // After nesting for a long time it becomes too much. Better to not use this way.
+    //   React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'testing'))
+
   );
 }
 
