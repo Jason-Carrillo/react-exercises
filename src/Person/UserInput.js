1ede0fe446e526a1101bf7a37cd9f7895e63b731
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-26 14:21:43 -0700

    added input, and output class information

---
+++ src/Person/UserInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class UserInput extends Component {
+    render() {
+        return (
+            <div className="UserInput">
+                <input/>
+            </div>
+        );
+    }
+
+}
+
+export default UserInput;
---
+++ src/Person/UserOutput.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class UserOutput extends Component {
+    render() {
+        return (
+            <div className="UserOutput">
+                <p> paragraph 1 </p>
+                <p> paragraph 2 </p>
+
+            </div>
+        );
+    }
+
+}
+
+export default UserOutput;
