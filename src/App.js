529dd28265d75f013d258fcf7c9044efa2a80d9c
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-27 11:01:57 -0700

    added additional files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { Components } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Components {
+class App extends Component {
     state = {
         persons: [
             {name: 'Jason', age: 20},
             {name: 'Testing', age: 22},
             {name: 'Test2', age: 18}
         ],
-        otherState: 'Other State'
+        otherState: 'Other State',
+        showPersons: false
     };
 
     switchNameHandler = (newName) => {
         // console.log("Was Clicked")
         // DONT DO THIS => state.persons[0].name = "Switched Jason";
-        this.setPersonsState({
+        this.setState({
             persons: [
                 {name: newName, age: 20},
                 {name: 'test2', age: 22},
@@ -35,9 +36,13 @@ class App extends Components {
         })
     }
 
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow});
+    }
 
-    render ()
-    {
+
+    render () {
 
         const style = {
             backgroundColor: 'white',
@@ -50,17 +55,23 @@ class App extends Components {
             <div className="App">
                 <h1>Hi, I'm Jason Carrillo</h1>
                 <p>I am a Software Developer</p>
-                <button style={style} onClick={() => this.switchNameHandler('Voorhees!!')}>Switch Name</button>
-                <Person
-                    name={this.state.persons[0].name}
-                    age={this.state.persons[0].age}/>
-                <Person
-                    name={this.state.persons[1].name}
-                    age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Voorhees")}
-                    changed={this.nameChangedHandler}>My Hobbies: Fishing</Person>
-                <Person
-                    name={this.state.persons[2].age}
-                    age={this.state.persons[2].age}/>
+                <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+                {
+                    this.state.showPersons ?
+                    <div>
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}/>
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Voorhees")}
+                            changed={this.nameChangedHandler}>My Hobbies: Fishing</Person>
+                        <Person
+                            name={this.state.persons[2].age}
+                            age={this.state.persons[2].age}/>
+                     </div>
+                        : null
+                }
             </div>
 
             // After nesting for a long time it becomes too much. Better to not use this way.
@@ -71,23 +82,3 @@ class App extends Components {
 }
 
 export default App;
-
-// state = {
-//     persons: [
-//         { name: 'Jason', age: 20},
-//         { name: 'Testing', age: 22},
-//         { name: 'Test2', age: 18}
-//     ],
-//     otherState: 'Other State'
-// }
-//
-// switchNameHandler = () => {
-//     // console.log("Was Clicked")
-//     // DONT DO THIS => state.persons[0].name = "Switched Jason";
-//     this.setState ({persons: [
-//             { name: 'SwitchedJason', age: 20},
-//             { name: 'Testing', age: 22},
-//             { name: 'SwitchedTest2', age: 18}
-//         ]
-//     })
-// }
