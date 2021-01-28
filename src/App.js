1b7da968e1388cc6e14e0e9681cc72e2c9419fb4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-28 11:29:51 -0700

    added if statements for style classes

--- src/App.js
+++ src/App.js
@@ -87,7 +87,13 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
-        let classes = ['red', 'bold'].join(' ')
+        let classes = []
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); //CLASSES WILL BE RED
+        }
+        if (this.state.persons.length <= 1){
+            classes.push('bold'); //CLASSES WILL BE BOLD
+        }
 
         return (
             <div className="App">
