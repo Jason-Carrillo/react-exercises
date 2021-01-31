e568d8b6c02d7ec00c6f003eb2b9dd109db926de
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-31 09:06:06 -0700

    added body to cockpit file

---
+++ src/Components/Cockpit/Cockpit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const cockpit = (props) => {
+    return (
+        <div>
+            <h1>Hi, I'm Jason Carrillo</h1>
+            <p className={assignedClasses.join(' ')}>I am a Software Developer</p>
+            <button className={btnClass} onClick={this.togglePersonsHandler}>Switch Name</button>
+        </div>
+    );
+}
