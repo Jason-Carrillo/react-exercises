4821240769b6d7c29c4b35dfdd4214cbb955f7db
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-30 22:48:44 -0700

    changed delete and  changed handler to props

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
 const persons = (props) => props.persons.map((person, index) => {
         return <Person
             key={person.id}
-            click={() => this.deletePersonHandler(index)}
+            click={() => props.deletePersonHandler(index)}
             name={person.name}
             age={person.age}
 
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
+            changed={(event) => props.nameChangedHandler(event, person.id)} />
 
     })
