0b3a5d2c178f544caebd3733272b9d0691fd967f
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-30 22:47:36 -0700

    reformatted function in persons

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
-const persons = (props) => (
-    persons.map((person, index) => {
-        return <ErrorBoundary key={person.id}>
-            <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
+const persons = (props) => props.persons.map((person, index) => {
+        return <Person
+            key={person.id}
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age}
 
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-            />
-        </ErrorBoundary>
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
+
     })
-)
