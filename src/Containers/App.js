b4d8c20ada258cb63c0a6d8f95f106a7a01eacfb
Jason Carrillo <jason.carrillo182@gmail.com>
2021-01-30 22:51:28 -0700

    added persons information from new comp

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -69,7 +69,9 @@ class App extends Component {
                     {
                         this.state.persons.map((person, index) => {
                             return <ErrorBoundary key={person.id}>
-                                <Person
+                                <Person persons={this.state.persons}
+                                        clicked={this.deletePersonHandler}
+                                        changed={this.nameChangedHandler}
                                 click={() => this.deletePersonHandler(index)}
                                 name={person.name}
                                 age={person.age}
