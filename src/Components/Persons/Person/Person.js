42b44610a76bddb0ff803b9dddc7b54ec74e67af
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-01 14:32:21 -0700

    changed Person to class instead of function

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css'
 
 
 
 
-const person = (props) => {
-    console.log('[Person.js] rendering...')
-    return (
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...')
+        return (
 
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm a {props.name}, and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed}/>
-        </div>
-    )
-}
+            <div className={classes.Person}>
+                <p onClick={props.click}>I'm a {props.name}, and I am {props.age} years old</p>
+                <p>{props.children}</p>
+                <input type="text" onChange={props.changed}/>
+            </div>
+        )
+    }
+};
 
-export default person;
+export default Person;
