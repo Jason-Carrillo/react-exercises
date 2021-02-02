a7c33fc9c14fc3de06429bd1c7664522273f2c6c
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 14:29:41 -0700

    changed persons to be personslength instead to only get changes that are related to it

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -32,10 +32,10 @@ const Cockpit = (props) => {
         btnClass = classes.Red;
     }
 
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
         assignedClasses.push(classes.red); //CLASSES WILL BE RED
     }
-    if (props.persons.length <= 1){
+    if (props.personsLength <= 1){
         assignedClasses.push(classes.bold); //CLASSES WILL BE BOLD
     }
 
