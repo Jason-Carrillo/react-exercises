f7814f7df7b7ff819ad8502bd6de676837451293
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 16:40:35 -0700

    added context authenticated

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -26,10 +26,9 @@ class Person extends Component {
         console.log('[Person.js] rendering...')
         return (
         <Aux>
-            <AuthContext.Consumer>
-                {(context) =>
-                    context.authenticated ? <p> Authenticated </p> : <p> Please log in</p>}
-            </AuthContext.Consumer>
+
+                {this.context.authenticated ? <p> Authenticated </p> : <p> Please log in</p>}
+
 
             <p key="i1" onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old
             </p>
