58cf800790ebd444a361696c20684a8d44b39577
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 14:44:49 -0700

    reformatted input for person

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -12,7 +12,11 @@ class Person extends Component {
             <div className={classes.Person}>
                 <p onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed}/>
+                <input
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
             </div>
         )
     }
