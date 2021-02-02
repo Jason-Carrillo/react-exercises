6fd0a2fad15702399a91c32d8f6ca1d06a21d959
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 15:46:02 -0700

    added input element focus on the last element

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -7,14 +7,20 @@ import withClass from '../../../hoc/withClass'
 
 
 class Person extends Component {
+    componentDidMount() {
+        this.inputElement.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...')
         return (
         <Aux>
-                <p onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old
+                <p key="i1" onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old
                 </p>
-                <p>{this.props.children}</p>
+                <p key="i2">{this.props.children}</p>
                 <input
+                    key="i3"
+                    ref={(inputEl) => {this.inputElement = inputEl} }
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
