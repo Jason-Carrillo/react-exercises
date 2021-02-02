3b64e6f81d151109f575785d1776bd7aee9f7d17
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 14:47:06 -0700

    added keys to person js

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -10,11 +10,11 @@ class Person extends Component {
         return [
 
 
-                <p
-                    onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old
+                <p key="i1" onClick={this.props.click}>
+                    I'm a {this.props.name}, and I am {this.props.age} years old
                 </p>,
-                <p>{this.props.children}</p>,
-                <input
+                <p key="i2">{this.props.children}</p>,
+                <input key="i3"
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
