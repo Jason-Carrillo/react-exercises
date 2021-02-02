537026a6b7b8677c9a564b20c65410987a919610
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-02 14:46:11 -0700

    changed return in Person to be an aarray

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -7,18 +7,19 @@ import classes from './Person.css'
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
-        return (
+        return [
 
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old</p>
-                <p>{this.props.children}</p>
+
+                <p
+                    onClick={this.props.click}>I'm a {this.props.name}, and I am {this.props.age} years old
+                </p>,
+                <p>{this.props.children}</p>,
                 <input
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </div>
-        )
+]
     }
 };
 
